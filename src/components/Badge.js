17fa435422a1da838d361b1bf0313fac3a24a6e7
Kevin Martello <kevinmartello@icloud.com>
2019-09-04 17:31:33 -0500

    Implementación de CSS en React
    import img, links y bootstrap

--- src/components/Badge.js
+++ src/components/Badge.js
@@ -1,25 +1,28 @@
 import React from 'react';
 import confLogo from '../images/badge-header.svg';
+import "./Badge.css"
 class Badge extends React.Component {
     render() {
-        return <div>
-            <div>
-                <img src={confLogo} alt="Logo de la conferencia" />
-            </div>
-            <div>
-                <img src="" alt="Avatar" />
+        return (
+            <div className="Badge">
+                <div className="Badge__header">
+                    <img src={confLogo} alt="Logo de la conferencia" />
+                </div>
+                <div className="Badge__section-name ">
+                    <img className="Badge__avatar" src="https://s.gravatar.com/avatar/d3a4caa3b2a1882555c69bbd0deaa005?s=80" alt="Avatar" />
 
-                <h1>Kevin <br />Martello</h1>
-            </div>
-            <div>
-                <p>Frontend Engineer</p>
-                <p>@kevinmartello</p>
-            </div>
-            <div>
-                #platziconf
+                    <h1>Kevin <br />Martello</h1>
+                </div>
+                <div className="Badge__section-info">
+                    <h2>Frontend Engineer</h2>
+                    <div>@kevinmartello</div>
+                </div>
+                <div className="Badge__footer">
+                    #platziconf
         </div>
 
-        </div>;
+            </div>
+        );
     }
 }
 
