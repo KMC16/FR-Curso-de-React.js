497d4d5c58310d283e6ea655657ec7d4c7e8c838
Kevin Martello <kevinmartello@icloud.com>
2019-09-03 23:40:05 -0500

    Commit inicial
    probando

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const element = <h1>Hola Mundo Provando React JSX</h1>
+const element = <h1>Hola Mundo, Provando React JSX, y el git</h1>
 const container = document.getElementById('app');
 // ReactDOM.render(__que__, __donde__);
 ReactDOM.render(element,container);
