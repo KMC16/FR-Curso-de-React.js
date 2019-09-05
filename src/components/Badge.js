5202296a68e347e3c413fa4ce81a8fa15de241d6
Kevin Martello <kevinmartello@icloud.com>
2019-09-05 11:57:56 -0500

    Props
    Props de nuestros componentes

--- src/components/Badge.js
+++ src/components/Badge.js
@@ -3,19 +3,22 @@ import confLogo from '../images/badge-header.svg';
 import "./Badge.css"
 class Badge extends React.Component {
     render() {
+
         return (
-            <div className="Badge">
+
+
+            <div className="Badge" >
                 <div className="Badge__header">
                     <img src={confLogo} alt="Logo de la conferencia" />
                 </div>
                 <div className="Badge__section-name ">
-                    <img className="Badge__avatar" src="https://s.gravatar.com/avatar/d3a4caa3b2a1882555c69bbd0deaa005?s=80" alt="Avatar" />
+                    <img className="Badge__avatar" src={this.props.avatar} alt="Avatar" />
 
-                    <h1>Kevin <br />Martello</h1>
+                    <h1>{this.props.firstName} <br />{this.props.lastName}</h1>
                 </div>
                 <div className="Badge__section-info">
-                    <h2>Frontend Engineer</h2>
-                    <div>@kevinmartello</div>
+                    <h2>{this.props.jobTitle}.</h2>
+                    <div>@{this.props.twitter}</div >
                 </div>
                 <div className="Badge__footer">
                     #platziconf
--- src/index.js
+++ src/index.js
@@ -6,4 +6,11 @@ import './global.css'
 
 const container = document.getElementById("app");
 // ReactDOM.render(__que__, __donde__);
-ReactDOM.render(<Badge />, container);
+
+ReactDOM.render(<Badge
+    firstName="Kevin Martello"
+    lastName="Mayorga Cleveland"
+    jobTitle="Ingeniero Mecatronico"
+    twitter="kevinmartello"
+    avatar="https://s.gravatar.com/avatar/d3a4caa3b2a1882555c69bbd0deaa005?s=80"
+/>, container);
